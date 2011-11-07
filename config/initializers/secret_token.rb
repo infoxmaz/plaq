ad641320b9531ef3ddbff335c835a2e80eeea837
Cozma Mazanov <admin@xmaz.ru>
2011-11-07 04:15:08 +0400

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plaq::Application.config.secret_token = '256b9f0e1c92919792392f802f1868116dc44719b3d06afc6f6e595285dfc2f747bbad868495a54bfa2dfe4c7e5c1fda5846193f71824b447c4492a0974898fc'
